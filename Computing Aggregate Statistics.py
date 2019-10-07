0bdc5d97467486c305bbbfb9ff4a77f05cd88321
Adavila8 <54561498+Adavila8@users.noreply.github.com>
2019-10-07 12:07:04 -0400

    Add files via upload

---
+++ Computing Aggregate Statistics.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+names = ['Bob','Jessica','Mary','John','Mel']
+grades = [76,95,77,78,99]
+bsdegrees = [1,1,0,0,1]
+msdegrees = [2,1,0,0,0]
+phddegrees = [0,1,0,0,0]
+
+
+# In[10]:
+
+
+Gradelist = zip(names,grades,bsdegrees,msdegrees,phddegrees)
+df = pd.DataFrame(data = Gradelist, columns=['Names', 'Grades','BS','MS','PhD'])
+
+
+# In[3]:
+
+
+df.count()
+
+
+# In[4]:
+
+
+df.mean()
+
+
+# In[5]:
+
+
+df.std()
+
+
+# In[6]:
+
+
+df.min()
+
+
+# In[7]:
+
+
+df.max()
+
+
+# In[8]:
+
+
+df.quantile(.25)
+
+
+# In[9]:
+
+
+df.quantile(.5)
+
+
+# In[11]:
+
+
+df.quantile(.75)
+
+
+# In[ ]:
+
+
+
+
